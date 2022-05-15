53b6dedfa0fb7a1a1c091d1e916a4f8e83303e55
Daniel Conlon <danielcconlon@gmail.com>
2022-05-15 11:42:17 -0400

    trying to fix for loop to display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ var questionObject = [
     },
 ];
 
+console.log(questionObject);
+
 // starting time
 var timeRemaining = 100;
 
@@ -42,6 +44,7 @@ var questionIndex = 0;
 
 var questionContainer = document.querySelector(".questions-container");
 var startQuiz = document.querySelector("#startQuiz");
+var counterDownTimerEl = document.querySelector("#countdown-timer")
 
 //  create a new unordered list element
 var buttonChoice = document.createElement("ul");
@@ -49,11 +52,11 @@ var buttonChoice = document.createElement("ul");
 
 
 var beginQuiz = function() {
-    startCountDown;
-    loadQuestions;
-    compareChoice;
+    startCountDown();
+    loadQuestions();
+    compareChoice();
 };
-
+console.log(beginQuiz);
 
 
 
@@ -83,16 +86,23 @@ var loadQuestions = function(questionIndex) {
     // need to clear button choices
     buttonChoice.innerHTML = "";
 
+
+    // var questionContainer = document.querySelector(".questions-container");
+    
     // loop through array to show question and responses
     for (var i = 0; i < questionObject.length; i++) {
         // defining the question
-        var targetQuestion = questionObject[questionIndex].questionPrompt;
+        var targetQuestion = document.createElement("p");
+        targetQuestion.textContent = questionObject[questionIndex].questionPrompt;
+        
 
         // defining the choices
         var questionOption = questionObject[questionIndex].choices;
 
         // appending the question to the page
-        questionContainer.textContent = targetQuestion;
+        questionContainer.appendChild(targetQuestion);
+
+
     }
 
     // go through each question choice
@@ -228,127 +238,6 @@ var finalStats = function() {
 
 // event listener to behing the quiz
 startQuiz.addEventListener("click", beginQuiz);
+console.log(startQuiz);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //pull data from object array (question and choices)
-// // bring in question
-// // for each or for loop  to iterate through and create button
-
-
-
-// // third function is getting the logic of right or wrong
-
-
-// // var arrayEx = [
-// //     {
-// //     question: 'kdfljhsglksdjf',
-// //     choices: ['option 1', 'option2', ]
-// //     }
-// // ]
-
-// // var currentQuestion = arrayEx[questionIndex]
-// // currentQuestion.choices.forEach(function(choice, i){
-// //      create button declare with a variable
-// //      set attribute to pull in value .setAttribute('value', choice)
-// //      append, text-content
-// //      button variable.textContent = i + 1 + '. ' + choice;
-// //      apply 'onclick' to the button variable
-// //      push the button variable
-// // })
-
-// // next function will be correct or wrong
-
-// // add event listening for start quiz button
-
-// // starting time
-// var timeRemaining = 100;
-
-// // keep track of the question we are in
-// var questionIndex = 0
-
-// var startQuizEl = document.querySelector("#start-quiz");
-// var counterDownTimerEl = document.querySelector("#countdown-timer")
-
-
-// var startQuiz = function() {
-//     hideIntro();
-//     startCountDown();
-//     questions();
-// };
-
-
-// // unhide question div
-// // hide  intro
-// var hideIntro = function() {
-//     // add class
-//     var hideDisplay = document.querySelector(".quiz-instructions-container");
-//     hideDisplay.classList.add("hide-display");
-// };
-
-// var handleAnswerChoice = function(event) {
-//     console.log(event);
-//     // defining what is being targeted when the user clicks a button
-//     var userChoice = event.target.textContent;
-//     var correctAnswer = questionObject[questionIndex].correctChoice
-
-//     if (correctAnswer === userChoice) {
-//         var displayCorrect = document.querySelector(".question-answers");
-//         displayCorrect.textContent = "Correct";
-//     }
-//     else {
-//         var displayWrong = document.querySelector(".question-answers");
-//         displayWrong.textContent = "Wrong";
-
-//         timeRemaining -= 10;
-//     }
-
-//     questionIndex ++;
-//     // reset to 0 when game finishes
-
-//     // call questions function to get next question
-//     questions();
-
-// };
-
-// var questions = function()  {
-//     var targetQuestion = document.querySelector(".questions-text");
-
-//     var questionText = document.createElement("h3");
-//     questionText.textContent = questionObject[questionIndex].questionPrompt;
-//     targetQuestion.appendChild(questionText);
-
-
-//     // for loop to go through all of the answer choices
-//     for (var i = 0; i < questionObject[questionIndex].choices.length; i++) {
-//         var answerButton = document.createElement("button");
-//             answerButton.textContent = questionObject[questionIndex].choices[i];
-
-//             // add event listener so the button can be clicked
-//             answerButton.onclick = handleAnswerChoice;
-
-//         // adding the buttons to the blank div
-//         var questionAnswers = document.querySelector(".question-answers");
-//             questionAnswers.appendChild(answerButton);
-
-//     }
-// };
-
-
-
-// startQuizEl.addEventListener("click", startQuiz);
